f7da5f272cfd825363ac5b1534562bd2b0945d8c
somakihiro <water0844@gmail.com>
2023-07-13 03:02:18 +0900

    楽天のレビュー情報を取得するためのスクレイピング追加

--- src/index.ts
+++ src/index.ts
@@ -52,3 +52,67 @@ http("scraping-rakuten-product-detail", async (req, res) => {
     },
   })
 })
+
+http("scraping-rakuten-product-reviews", async (req, res) => {
+  const body = JSON.parse(req.body)
+  const options =
+    process.env.NODE_ENV === "production"
+      ? {
+          args: chromium.args,
+          executablePath: await chromium.executablePath,
+          headless: chromium.headless,
+        }
+      : {
+          args: [],
+          executablePath:
+            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+          headless: false,
+        }
+
+  const browser = await puppeteer.launch(options)
+  const page = await browser.newPage()
+  page.setDefaultNavigationTimeout(0)
+  await page.goto(body.siteUrl)
+  await page.waitForSelector(".button--3SNaj") // レビューボタンが表示されるまで待つ
+  await page.click('a[href^="https://review.rakuten.co.jp"]')
+
+  const reviewSortBtnClassName = ".revRvwSortTurn"
+  // 参考になるレビュー順で表示するため、ソートボタンが表示されるまで待つ
+  await page.waitForSelector(reviewSortBtnClassName)
+  await page.click('a[l2id_linkname="search_03"]')
+
+  // 同じページの遷移のため、documentをリセットするためにページをリロードする
+  // リロードしないと、遷移前のレビューを取得してしまう
+  await page.reload()
+  const reviewItemClassName = ".revRvwUserEntryCmt"
+
+  // レビューが表示されるまで待つ
+  await page.waitForSelector(reviewSortBtnClassName)
+
+  // 総合評価の取得
+  const comprehensiveEvalElement = await page.$(".revEvaNumber")
+  const comprehensiveEval = await (
+    await comprehensiveEvalElement?.getProperty("innerText")
+  )?.jsonValue()
+
+  // 評価件数の取得
+  const totalEvalCountElement = await page.$(".revEvaCount > .Count")
+  const totalEvalCount = await (
+    await totalEvalCountElement?.getProperty("innerText")
+  )?.jsonValue()
+
+  // 1ページ目のレビューの取得（最大15件）
+  const list = await page.$$(reviewItemClassName)
+  let reviews = []
+  for (let i = 0; i < list.length; i++) {
+    reviews.push(await (await list[i].getProperty("textContent"))?.jsonValue())
+  }
+
+  await browser.close()
+
+  res.status(200).json({
+    comprehensiveEval,
+    totalEvalCount,
+    reviews,
+  })
+})
